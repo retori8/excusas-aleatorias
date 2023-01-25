a815f2d53f1ffb49b66261e7a8a042ac7f55abfe
retori8 <reciclee@gmail.com>
2023-01-25 00:12:14 +0000

    ejercicio terminad

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//generar seleccion de una variable aleatoria//
+function getRandomIntInclusive(d, e) {
+  return Math.floor(Math.random() * (e - d + 1) + 0);
+}
+let getexcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindex = getRandomIntInclusive(0, 3);
+  console.log(whoindex);
+  let whatindex = getRandomIntInclusive(0, 3);
+  console.log(whatindex);
+  let whenindex = getRandomIntInclusive(0, 4);
+  console.log(whenindex);
+
+  return who[whoindex] + " " + what[whatindex] + " " + when[whenindex];
+};
+window.onload = () => {
+  document.querySelector("#excuseId").innerHTML = getexcuse();
 };
